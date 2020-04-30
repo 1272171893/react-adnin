fb6b63193ed0f5c4323f6a89f197e38c9691a01f
小鱼人 <1272171893@qq.com>
2020-04-30 15:48:55 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './view/home/index';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
