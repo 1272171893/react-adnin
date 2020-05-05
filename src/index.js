1829259e58eab61b42f713b81d0353f68f7497a2
“1272171893 <123456q1272171893@qq.com>
2020-05-05 15:59:56 +0800

    登录页面

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "./assets/css/minreset.css"
+import "./assets/css/minreset.less"
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import  "./less/index.less";
+import React from "react";
+import "./less/index.less";
+import logo from "@/assets/image/logo.png";
 export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        return (
-            <div className='login'>
-                <h1>hellow world login</h1>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      layout: {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+      },
+    };
+  }
+  render() {
+    return (
+      <div className="login-container">
+        <div className="header-container">
+          <img src={logo} alt="后太登录页面" />
+          <h1>后台登录页面</h1>
+        </div>
+        <div className="form-container">
+          <div className="title">欢迎登陆</div>
+          <div className="main">
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
