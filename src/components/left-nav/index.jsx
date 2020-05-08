523b594c40ae231eb1edb72f99da505652f5cdf9
“1272171893 <123456q1272171893@qq.com>
2020-05-09 00:11:27 +0800

    左侧的菜单的使用

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,15 +1,69 @@
-import React from 'react';
+import React from "react";
 import "./index.less";
+import { Link } from "react-router-dom";
+import logo from "@/assets/image/logo.png";
+import { Menu } from "antd";
+import menuList from "@/config/menuConfig";
+const { SubMenu } = Menu;
+
 export default class LeftNav extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  getMenuNodes = (values) => {
+    return values.map((item) => {
+      if (item.children && item.children.length !== 0) {
         return (
-            <div className='left-nav'>
-               hellow world
-            </div>
-        );
-    }
+            <SubMenu key={item.key}  title={item.title}>
+                {this.getMenuNodes(item.children)}
+            </SubMenu>
+          );
+      }
+      return (
+        <Menu.Item key={item.key}>
+          <Link to={item.key}>
+            <span>{item.title}</span>
+          </Link>
+        </Menu.Item>
+      );
+    });
+  };
+  render() {
+    return (
+      <div className="left-nav">
+        <Link className="left-nav-link" to="/home">
+          <img src={logo} alt="" />
+          <h1>后台管理</h1>
+        </Link>
+        <Menu
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={this.state.collapsed}
+        >
+          {this.getMenuNodes(menuList)}
+          {/* <Menu.Item key="/home" >
+            <Link to="/home">
+              <span>首页</span>
+            </Link>
+          </Menu.Item>
+
+          <SubMenu key="sub1"  title="Navigation One">
+            <Menu.Item key="/category" >
+              <Link to="/category">
+                <span>品类管理</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="/product" >
+              <Link to="/product">
+                <span>商品管理</span>
+              </Link>
+            </Menu.Item> */}
+          {/* </SubMenu> */}
+        </Menu>
+      </div>
+    );
+  }
 }
---
+++ src/config/menuConfig.js
@@ -0,0 +1,24 @@
+const menuList = [{
+        title: "首页",
+        key: "/home"
+    },
+    {
+        title: "商品",
+        key: "/products",
+        children: [{
+            title: "品类管理",
+            key: "/category",
+        },
+        {
+            title: "商品管理",
+            key: "/product",
+        }]
+    }
+
+
+
+
+
+]
+
+export default menuList
