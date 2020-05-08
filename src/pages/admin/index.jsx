726b5f9b0d1ef30d8f14e4ceb347980ee454f53d
“1272171893 <123456q1272171893@qq.com>
2020-05-08 23:28:40 +0800

    update

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, Switch, Route } from "react-router-dom";
 import utils from "@/utils/storageUtils";
 import Header from "@/components/header";
 import LeftNav from "@/components/left-nav";
+import Home from "@/pages/home";
+import Category from "@/pages/category";
+import Product from "@/pages/product";
+import Role from "@/pages/role";
+import User from "@/pages/user";
 import { Layout } from "antd";
 const { Footer, Sider, Content } = Layout;
 export default class Admin extends React.Component {
@@ -22,8 +27,20 @@ export default class Admin extends React.Component {
         </Sider>
         <Layout>
           <Header></Header>
-          <Content style={{background:"#fff"}}>Content</Content>
-          <Footer style={{textAlign:"center",color:"rgba(0,0,0,.5)"}}>Footer</Footer>
+          <Content style={{ background: "#fff" }}>
+            {/* <Home></Home> */}
+            <Switch>
+              <Route path="/home" component={Home}></Route>
+              <Route path="/category" component={Category}></Route>
+              <Route path="/product" component={Product}></Route>
+              <Route path="/role" component={Role}></Route>
+              <Route path="/user" component={User}></Route>
+              <Redirect to="/home"></Redirect>
+            </Switch>
+          </Content>
+          <Footer style={{ textAlign: "center", color: "rgba(0,0,0,.5)" }}>
+            Footer
+          </Footer>
         </Layout>
       </Layout>
     );
